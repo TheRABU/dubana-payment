2cb3ce7a538cc854f81739890986ae8f0df0eafe
TheRABU <mdfazlerabbi114@gmail.com>
2025-10-17 22:32:53 +0600

    added a backdrop-blur effect on PaymentModal

--- src/pages/PaymentModal.jsx
+++ src/pages/PaymentModal.jsx
@@ -82,7 +82,7 @@ const PaymentModal = ({
   const installmentDetails = getInstallmentDetails();
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 backdrop-blur-xs flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto relative">
         <div className="border-b-2 border-slate-400 p-6 relative">
           <button
